43427883e132af16becb70a7ffe4834ed6bb0a5e
Reza Juliandri <rezajuliandri20@gmail.com>
2020-05-29 02:20:52 +0700

    [Update] Bypass login checkpoint

--- run.php
+++ run.php
@@ -1,10 +1,15 @@
 <?php
 
+use InstagramAPI\Exception\ChallengeRequiredException;
 use InstagramAPI\Instagram;
 
 require("lib/config.php");
 require("lib/functions.php");
 require('lib/cli.php');
+
+$verification_method = 1; 	//0 = SMS, 1 = Email
+
+
 
 $connection = [
     'storage' => 'mysql',
@@ -16,7 +21,23 @@ $connection = [
 if (!$saveToDB) {
     $connection = [];
 }
-$ig = new Instagram($debug, $truncatedDebug, $connection);
+class ExtendedInstagram extends Instagram {
+    public function changeUser( $username, $password ) {
+        $this->_setUser( $username, $password );
+    }
+}
+
+function readln( $prompt ) {
+    if ( PHP_OS === 'WINNT' ) {
+        echo "$prompt ";
+
+        return trim( (string) stream_get_line( STDIN, 6, "\n" ) );
+    }
+
+    return trim( (string) readline( "$prompt " ) );
+}
+
+$ig = new ExtendedInstagram($debug, $truncatedDebug, $connection);
 try {
     $ig->login($username, $password);
     foreach ($showStoryUsername as $story_username):
@@ -48,6 +69,57 @@ try {
     endforeach;
 } catch (\Exception $e) {
     echo 'Something went wrong: ' . $e->getMessage() . "\n";
-    exit(0);
+    $response = $e->getResponse();
+
+    if ($e instanceof ChallengeRequiredException
+        && $response->getErrorType() === 'checkpoint_challenge_required') {
+
+        sleep(3);
+
+        $checkApiPath = substr($response->getChallenge()->getApiPath(), 1);
+        $customResponse = $ig->request($checkApiPath)
+            ->setNeedsAuth(false)
+            ->addPost('choice', $verification_method)
+            ->addPost('_uuid', $ig->uuid)
+            ->addPost('guid', $ig->uuid)
+            ->addPost('device_id', $ig->device_id)
+            ->addPost('_uid', $ig->account_id)
+            ->addPost('_csrftoken', $ig->client->getToken())
+            ->getDecodedResponse();
+
+    } else {
+        echo "Not a challenge required exception...\n";
+        exit;
+    }
+
+    try {
+
+        if ($customResponse['status'] === 'ok' && $customResponse['action'] === 'close') {
+            echo 'Checkpoint bypassed';
+            exit();
+        }
+
+        $code = readln('Code that you received via ' . ($verification_method ? 'email' : 'sms') . ':');
+        $ig->changeUser($username, $password);
+        $customResponse = $ig->request($checkApiPath)
+            ->setNeedsAuth(false)
+            ->addPost('security_code', $code)
+            ->addPost('_uuid', $ig->uuid)
+            ->addPost('guid', $ig->uuid)
+            ->addPost('device_id', $ig->device_id)
+            ->addPost('_uid', $ig->account_id)
+            ->addPost('_csrftoken', $ig->client->getToken())
+            ->getDecodedResponse();
+
+        if ($customResponse['status'] === 'ok' && (int)$customResponse['logged_in_user']['pk'] === (int)$user_id) {
+            echo 'Finished, logged in successfully! Run this file again to validate that it works.';
+        } else {
+            echo "Probably finished...\n";
+            var_dump($customResponse);
+        }
+    } catch (Exception $ex) {
+        echo $ex->getMessage();
+    }
+//    exit(0);
 }
   
